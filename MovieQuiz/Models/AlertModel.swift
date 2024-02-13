32293baada90bc3d338974753da4c9acfb398777
KirillKaszycki <kashitskii.kirill@gmail.com>
2024-02-13 18:46:44 +0500

    created AlertPresenter, AlertModel

---
+++ MovieQuiz/Models/AlertModel.swift
@@ -0,0 +1,8 @@
+//
+//  AlertModel.swift
+//  MovieQuiz
+//
+//  Created by Кирилл Кашицкий on 13.02.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  AlertPresenter.swift
+//  MovieQuiz
+//
+//  Created by Кирилл Кашицкий on 13.02.2024.
+//
+
+import Foundation
