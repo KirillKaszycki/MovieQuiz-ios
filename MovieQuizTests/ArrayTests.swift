363dad147f21706c33232c28d6fc43526ba14ad3
KirillKaszycki <kashitskii.kirill@gmail.com>
2024-03-08 15:34:20 +0500

    Initial commit

---
+++ MovieQuizTests/ArrayTests.swift
@@ -0,0 +1,8 @@
+//
+//  ArrayTests.swift
+//  MovieQuizTests
+//
+//  Created by Кирилл Кашицкий on 08.03.2024.
+//
+
+import Foundation
---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesLoaderTests.swift
+//  MovieQuizTests
+//
+//  Created by Кирилл Кашицкий on 08.03.2024.
+//
+
+import Foundation
