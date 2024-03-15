e08e0c02af8b030d911f160d6e3823b5d0f8bee3
KirillKaszycki <kashitskii.kirill@gmail.com>
2024-03-15 18:52:49 +0500

    Completely refactored the logic of several functions, edited the allert and result show objects, corrected the button block logic with GCD, added the tests

---
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizPresenter.swift
+//  MovieQuiz
+//
+//  Created by Кирилл Кашицкий on 14.03.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Presentation/MovieQuizViewControllerProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizViewControllerProtocol.swift
+//  MovieQuiz
+//
+//  Created by Кирилл Кашицкий on 15.03.2024.
+//
+
+import Foundation
